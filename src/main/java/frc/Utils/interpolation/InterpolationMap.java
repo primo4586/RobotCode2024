adacd1fbb075dc5170aedd730484813a96676176
kully <oricoval@gmail.com>
2024-01-04 18:50:31 +0200

    added interpolation

---
+++ src/main/java/frc/Utils/interpolation/InterpolationMap.java
@@ -0,0 +1,31 @@
+package frc.Utils.interpolation;
+
+import java.util.HashMap;
+
+/**
+ * Represents a map of data to interpolated from to, (Shooter speeds for
+ * specific distances, etc.)
+ * 
+ * @see InterpolateUtil
+ */
+public class InterpolationMap {
+
+    private HashMap<Double, Double> dataPoints;
+
+    public InterpolationMap() {
+        this.dataPoints = new HashMap<>();
+    }
+
+    public InterpolationMap put(double x, double value) {
+        dataPoints.put(x, value);
+        return this;
+    }
+
+    public double get(double x) {
+        return dataPoints.getOrDefault(x, -Double.MAX_VALUE);
+    }
+
+    public HashMap<Double, Double> getDataPoints() {
+        return dataPoints;
+    }
+}

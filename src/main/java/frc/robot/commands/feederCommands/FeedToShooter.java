1ffd6decd492ee6427f3936e0ccd6e8e30cd679f
kully <oricoval@gmail.com>
2024-01-22 15:12:38 +0200

    stop feeder at the end

--- src/main/java/frc/robot/commands/feederCommands/FeedToShooter.java
+++ src/main/java/frc/robot/commands/feederCommands/FeedToShooter.java
@@ -38,7 +38,9 @@ public class FeedToShooter extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    feederSubsystem.setSpeed(0);
+  }
 
   // Returns true when the command should end.
   @Override

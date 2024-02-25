28e15d6b1d2096713bc163d34bab5599d3fba99f
0kullyThekoala0 <oricoval@gmail.com>
2024-02-25 11:47:12 +0200

    proper trigger joystic implemntation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.Utils.PathPlanner.PathPlannerHelper;
 import frc.robot.Constants.IntakeArmConstants;
 import frc.robot.aRobotOperations.CollectToFeeder;
@@ -33,9 +34,12 @@ import frc.robot.basicCommands.feederCommands.FeederSetSpeed;
 import frc.robot.subsystems.*;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
@@ -44,10 +48,23 @@ public class RobotContainer {
     private final CommandXboxController operator = new CommandXboxController(1);
     private final CommandXboxController test = new CommandXboxController(2);
 
-    /* Drive Controls */
-    private final int translationAxis = XboxController.Axis.kLeftY.value;
-    private final int strafeAxis = XboxController.Axis.kLeftX.value;
-    private final int rotationAxis = XboxController.Axis.kRightX.value;
+    // Driver Triggers
+    Trigger driverYTrigger = driver.y();
+    Trigger driverXTrigger = driver.x();
+    Trigger driverRightBumperTrigger = driver.rightBumper();
+    Trigger driverLeftTriggerToggle = driver.leftTrigger();
+    Trigger driverBackTrigger = driver.back();
+    Trigger driverPovLeftTrigger = driver.povLeft();
+    Trigger driverPovRightTrigger = driver.povRight();
+
+    // Operator Triggers
+    Trigger operatorXTrigger = operator.x();
+    Trigger operatorYTrigger = operator.y();
+    Trigger operatorATrigger = operator.a();
+    Trigger operatorRightBumperTrigger = operator.rightBumper();
+    Trigger operatorLeftBumperTrigger = operator.leftBumper();
+    Trigger operatorBTrigger = operator.b();
+
     PathPlannerHelper pathPlannerHelper = PathPlannerHelper.getInstace();
 
     /* Subsystems */
@@ -55,39 +72,34 @@ public class RobotContainer {
     private final IntakeSubsystem intake = IntakeSubsystem.getInstance();
     private final IntakeArmSubsystem intakeArm = IntakeArmSubsystem.getInstance();
 
-
-    /** The container for the robot. Contains subsystems, OI devices, and commands. */
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
     public RobotContainer() {
         swerve.setDefaultCommand(
-             new TeleopSwerve(
-                 swerve, 
-                 () -> -driver.getRawAxis(translationAxis), 
-                 () -> -driver.getRawAxis(strafeAxis), 
-                 () -> -driver.getRawAxis(rotationAxis), 
-                 true,
-                 () -> driver.rightTrigger().getAsBoolean()
-             )
-        );
+                new TeleopSwerve(
+                        swerve,
+                        () -> driver.getHID().getLeftY(),
+                        () -> driver.getHID().getLeftX(),
+                        () -> -driver.getHID().getRightX(),
+                        true,
+                        () -> driver.getHID().getRightTriggerAxis() > 0.5));
 
-        driver.leftTrigger().onTrue(PathPlannerHelper.getInstace().followPath("testSpin"));
+        // IntakeArmSubsystem.getInstance().setDefaultCommand(new IntakeArmSetSpeed(()->
+        // -test.getRightX()));
 
-
-        //IntakeArmSubsystem.getInstance().setDefaultCommand(new IntakeArmSetSpeed(()-> -test.getRightX()));
-
-
-        
         // ShooterArmSubsystem.getInstance().setDefaultCommand(new InstantCommand(()->{
-        //      ShooterArmSubsystem.getInstance().setSpeedArm(()-> test.getLeftX() / 10);
-        //  }, ShooterArmSubsystem.getInstance()));
+        // ShooterArmSubsystem.getInstance().setSpeedArm(()-> test.getLeftX() / 10);
+        // }, ShooterArmSubsystem.getInstance()));
         // test.b().onTrue(new ZeroShooterArm());
 
         // ShooterSubsystem.getInstance().setDefaultCommand(new InstantCommand(()->{
-        //     ShooterSubsystem.getInstance().manualSetShooterSpeed(()-> test.getRightY());
+        // ShooterSubsystem.getInstance().manualSetShooterSpeed(()-> test.getRightY());
         // }, ShooterSubsystem.getInstance()));
 
-        // IntakeSubsystem.getInstance().setDefaultCommand(new IntakeSetSpeed(()-> -test.getLeftY()));
+        // IntakeSubsystem.getInstance().setDefaultCommand(new IntakeSetSpeed(()->
+        // -test.getLeftY()));
 
-        
         // test.a().onTrue(new CollectToIntake());
 
         // Configure the button bindings
@@ -96,47 +108,62 @@ public class RobotContainer {
         // driver.leftBumper().onTrue(new FeederSetSpeed(()->1));
         // test.rightBumper().onTrue(new FeederSetSpeed(()->0));
         // driver.start().onTrue(new ZeroShooterArm());
-        // driver.x().whileTrue(new InstantCommand(()-> ShooterSubsystem.getInstance().setShooterSpeed(100,100), ShooterSubsystem.getInstance()));
-        // test.start().whileTrue(new InstantCommand(()-> ShooterSubsystem.getInstance().setShooterSpeed(0, 0), ShooterSubsystem.getInstance()));
-        
+        // driver.x().whileTrue(new InstantCommand(()->
+        // ShooterSubsystem.getInstance().setShooterSpeed(100,100),
+        // ShooterSubsystem.getInstance()));
+        // test.start().whileTrue(new InstantCommand(()->
+        // ShooterSubsystem.getInstance().setShooterSpeed(0, 0),
+        // ShooterSubsystem.getInstance()));
+
         // driver.leftTrigger().whileTrue(new AlignToSpeaker());
-        //driver.x().onTrue(new ShooterArmSpeakerAngle());
+        // driver.x().onTrue(new ShooterArmSpeakerAngle());
         // driver.b().onTrue(new CollectToFeeder());
         // driver.a().onTrue(new ShootSpeaker());
 
-        // // test.rightBumper().whileTrue(new InstantCommand(()-> ShooterSubsystem.getInstance().setShooterSpeed(40, 40), ShooterSubsystem.getInstance()));
-        test.b().onTrue(new IntakeArmUP());
-        test.x().onTrue(new IntakeArmDown());
-        test.a().onTrue(new ZeroIntakeArm());
-        // // test.leftBumper().onTrue(new ShootTouchingBase());
+        // // test.rightBumper().whileTrue(new InstantCommand(()->
+        // ShooterSubsystem.getInstance().setShooterSpeed(40, 40),
+        // ShooterSubsystem.getInstance()));
+        // test.b().onTrue(new IntakeArmUP());
+        // test.x().onTrue(new IntakeArmDown());
 
+        driver.a().whileTrue(new InstantCommand(() -> ShooterSubsystem.getInstance().setShooterSpeed(100, 100),
+                ShooterSubsystem.getInstance()));
+        // // test.leftBumper().onTrue(new ShootTouchingBase());
 
         configureButtonBindings();
     }
 
     /**
-     * Use this method to define your button->command mappings. Buttons can be created by
+     * Use this method to define your button->command mappings. Buttons can be
+     * created by
      * 
      * instantiating a {@link GenericHID} or one of its subclasses ({@link
-     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+     * it to a {@link
      * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
      */
     private void configureButtonBindings() {
-        /* Driver Buttons */
-        driver.y().onTrue(new InstantCommand(() -> swerve.zeroGyro()));
-        driver.x().onTrue(new ShootTouchingBase());
-        driver.rightTrigger().onTrue(new ShootSpeaker());
-        driver.leftTrigger().toggleOnTrue(new AlignToSpeaker());
 
+        // Driver Button Bindings
+        driverYTrigger.onTrue(new InstantCommand(() -> swerve.zeroGyro()));
+        driverXTrigger.onTrue(new ShootTouchingBase());
+        driverRightBumperTrigger.onTrue(new ShootSpeaker().finallyDo(() -> ShooterSubsystem.getInstance().coast()));
+        driverLeftTriggerToggle.toggleOnTrue(new AlignToSpeaker());
+        driverBackTrigger.onTrue(swerve.disableHeadingCommand());
+        driverPovLeftTrigger.onTrue(new ZeroIntakeArm());
+        driverPovRightTrigger.onTrue(new ZeroShooterArm());
 
-        /* Operator Buttons */
-        operator.x().onTrue(new CollectToFeeder());
-        operator.y().toggleOnTrue(new PrepareForShoot());
-        operator.a().onTrue(Commands.runOnce(()-> intakeArm.moveArmTo(IntakeArmConstants.SafeSetPoint), intakeArm));
-        operator.rightBumper().onTrue(new IntakeArmDown());
-        operator.leftBumper().onTrue(new IntakeArmUP());
-        
-        IntakeSubsystem.getInstance().setDefaultCommand(new IntakeSetSpeed(()-> -test.getLeftY()));
-        FeederSubsystem.getInstance().setDefaultCommand(new FeederSetSpeed(()-> -test.getLeftY()));
+        // Operator Button Bindings
+        operatorXTrigger.onTrue(new CollectToFeeder());
+        operatorYTrigger.toggleOnTrue(new PrepareForShoot().repeatedly());
+        operatorATrigger
+                .onTrue(Commands.runOnce(() -> intakeArm.moveArmTo(IntakeArmConstants.SafeSetPoint), intakeArm));
+        operatorRightBumperTrigger.onTrue(new IntakeArmDown());
+        operatorLeftBumperTrigger.onTrue(new IntakeArmUP());
+        operatorBTrigger.onTrue(
+                new InstantCommand(() -> ShooterSubsystem.getInstance().coast(), ShooterSubsystem.getInstance()));
+
+        IntakeSubsystem.getInstance().setDefaultCommand(new IntakeSetSpeed(() -> -operator.getHID().getLeftY()));
+        FeederSubsystem.getInstance().setDefaultCommand(new FeederSetSpeed(() -> -operator.getHID().getLeftY()));
     }
 }

ec69180c13cba9ea32baa8d43cff93d7b4ce7071
kully <oricoval@gmail.com>
2024-04-30 17:50:08 +0300

    ctre base code

--- src/main/java/frc/robot/MiscConstants.java
+++ src/main/java/frc/robot/MiscConstants.java
@@ -4,12 +4,9 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.RobotBase;
 
-public final class Main {
-  private Main() {}
-
-  public static void main(String... args) {
-    RobotBase.startRobot(Robot::new);
-  }
+/** Add your docs here. */
+public interface MiscConstants {
+    
+    String kCANbusName = "CANBUS";
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,110 +1,77 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.a_robotCommandGroups.ReadyShootSpeaker;
-import frc.robot.a_robotCommandGroups.ShootBase;
-import frc.robot.a_robotCommandGroups.ShootSpeaker;
-import frc.robot.a_robotCommandGroups.ShootStage;
-import frc.robot.basicCommands.ShooterArmCommands.ZeroShooterArm;
-import frc.robot.basicCommands.ShooterCommands.ShooterSetSpeedForever;
-import frc.robot.basicCommands.SwerveCommands.AutoAlignToSpeaker;
-import frc.robot.basicCommands.SwerveCommands.TeleopSwerve;
-import frc.robot.basicCommands.TakeFeedCommands.CollectUntilNote;
-import frc.robot.basicCommands.TakeFeedCommands.TakeFeedJoystickSetSpeed;
-import frc.robot.subsystems.shooter.ShooterConstants;
-import frc.robot.subsystems.shooter.ShooterSubsystem;
-import frc.robot.subsystems.swerve.SwerveSubsystem;
-import frc.robot.subsystems.takeFeed.TakeFeedSubsystem;
-import frc.util.PathPlanner.PathPlannerHelper;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import frc.robot.subsystems.swerve.CommandSwerveDrivetrain;
+import frc.robot.subsystems.swerve.Telemetry;
+import frc.robot.subsystems.swerve.TunerConstants;
 
-/**
- * This class is where the bulk of the robot should be declared. Since
- * Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in
- * the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of
- * the robot (including
- * subsystems, commands, and button mappings) should be declared here.
- */
 public class RobotContainer {
-    /* Controllers */
-    private final CommandXboxController driver = new CommandXboxController(0);
-    private final CommandXboxController operator = new CommandXboxController(1);
+  private double MaxSpeed = TunerConstants.kSpeedAt12VoltsMps; // kSpeedAt12VoltsMps desired top speed
+  private double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
 
-    // Driver Triggers
-    Trigger driverYTrigger = driver.y();
-    Trigger driverXTrigger = driver.x();
-    Trigger driverBTrigger = driver.b();
-    Trigger driverRightBumperTrigger = driver.rightBumper();
-    Trigger driverLeftTriggerToggle = driver.leftTrigger();
-    Trigger driverBackTrigger = driver.back();
-    Trigger driverPovLeftTrigger = driver.povLeft();
-    Trigger driverPovRightTrigger = driver.povRight();
-    Trigger driverStart = driver.start();
+  /* Setting up bindings for necessary control of the swerve drive platform */
+  private final CommandXboxController joystick = new CommandXboxController(0); // My joystick
+  public final CommandSwerveDrivetrain drivetrain = TunerConstants.DriveTrain; // My drivetrain
 
-    // Operator Triggers
-    Trigger operatorXTrigger = operator.x();
-    Trigger operatorYTrigger = operator.y();
-    Trigger operatorATrigger = operator.a();
-    Trigger operatorRightBumperTrigger = operator.rightBumper();
-    Trigger operatorLeftBumperTrigger = operator.leftBumper();
-    Trigger operatorBTrigger = operator.b();
-    Trigger operatorStart = operator.start();
+  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+      .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+      .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // I want field-centric
+                                                               // driving in open loop
+  private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+  private final SwerveRequest.RobotCentric forwardStraight = new SwerveRequest.RobotCentric().withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+  private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
-    PathPlannerHelper pathPlannerHelper = PathPlannerHelper.getInstace();
+  /* Path follower */
+  private Command runAuto = drivetrain.getAutoPath("Tests");
 
-    /* Subsystems */
-    private final SwerveSubsystem swerve = SwerveSubsystem.getInstance();
+  private final Telemetry logger = new Telemetry(MaxSpeed);
 
-    /**
-     * The container for the robot. Contains subsystems, OI devices, and commands.
-     */
-    public RobotContainer() {
-        swerve.setDefaultCommand(
-                new TeleopSwerve(
-                        () -> -driver.getHID().getLeftY(),
-                        () -> -driver.getHID().getLeftX(),
-                        () -> -driver.getHID().getRightX(),
-                        true,
-                        () -> driver.getHID().getRightTriggerAxis() > 0.5));
+  private void configureBindings() {
+    drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
+        drivetrain.applyRequest(() -> drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with
+                                                                                           // negative Y (forward)
+            .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+            .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+        ).ignoringDisable(true));
+
+    joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+    joystick.b().whileTrue(drivetrain
+        .applyRequest(() -> point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
+
+    // reset the field-centric heading on left bumper press
+    joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()) );
+
+    drivetrain.registerTelemetry(logger::telemeterize);
+
+    joystick.pov(0).whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(0.5).withVelocityY(0)));
+    joystick.pov(180).whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(-0.5).withVelocityY(0)));
 
 
-        configureButtonBindings();
-    }
+    /* Bindings for drivetrain characterization */
+    /* These bindings require multiple buttons pushed to swap between quastatic and dynamic */
+    /* Back/Start select dynamic/quasistatic, Y/X select forward/reverse direction */
+    joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+    joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+    joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+    joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+  }
 
-    /**
-     * Use this method to define your button->command mappings. Buttons can be
-     * created by
-     * 
-     * instantiating a {@link GenericHID} or one of its subclasses ({@link
-     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
-     * it to a {@link
-     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-     */
-    private void configureButtonBindings() {
+  public RobotContainer() {
+    configureBindings();
+  }
 
-        // Driver Button Bindings
-        // pathPlanner.followChoreoPath("2 to 1", false)
-        //driverStart.onTrue(new EStop());
-        driverYTrigger.onTrue(new InstantCommand(() -> swerve.zeroGyro()));
-        driverXTrigger.onTrue(new ShootBase());
-        driverBTrigger.onTrue(new ShootStage());
-        driverRightBumperTrigger.whileTrue(new ShootSpeaker());
-        driverLeftTriggerToggle.toggleOnTrue(new AutoAlignToSpeaker());
-        driverBackTrigger.onTrue(swerve.disableHeadingCommand());
-        driverPovRightTrigger.onTrue(new ZeroShooterArm());
-
-        // // Operator Button Bindings
-        // operatorRightBumperTrigger.whileTrue(new ShooterSetSpeedForever(ShooterConstants.shooterConstants.SHOOT_SPEED));
-        // operatorLeftBumperTrigger.onTrue(new InstantCommand(()->ShooterSubsystem.getInstance().coast()));
-        //operatorStart.onTrue(new EStop());
-        operatorXTrigger.onTrue(new CollectUntilNote());
-        operatorYTrigger.onTrue(new ReadyShootSpeaker());
-        operatorBTrigger.onTrue(new InstantCommand(() -> ShooterSubsystem.getInstance().coast(), ShooterSubsystem.getInstance()));
-        TakeFeedSubsystem.getInstance().setDefaultCommand(new TakeFeedJoystickSetSpeed(()-> - operator.getHID().getLeftY()));
-    }
+  public Command getAutonomousCommand() {
+    /* First put the drivetrain into auto run mode, then run the auto */
+    return runAuto;
+  }
 }
